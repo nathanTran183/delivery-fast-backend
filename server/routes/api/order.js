e5eab7cea086746cfb9393faf2d8f6cea946e8c6
Nathan Tran <phuctd1803@gmail.com>
2017-11-15 17:51:46 +0700

    remove unused code

--- server/routes/api/order.js
+++ server/routes/api/order.js
@@ -6,7 +6,7 @@ const ordersController = require('../../controllers/api').orders;
 const router = express.Router();
 const passport = require('../../middlewares/passport');
 
-router.get('/submitted', Z ordersController.getSubmittedList);
+router.get('/submitted', passport.notUserWeb, ordersController.getSubmittedList);
 router.get('/', ordersController.list);
 
 router.get('/history', passport.isUserAPI, ordersController.history);
